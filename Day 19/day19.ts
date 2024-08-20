7c07f4f391f11ce6daf5c912de0ca3d76e2badc1
Muhammad Awais <162321104+mhdawaisrajput@users.noreply.github.com>
2024-08-20 22:52:09 +0500

    code uploaded😍

---
+++ Day 19/day19.ts
@@ -0,0 +1,16 @@
+🚀 *Day 19 Challenge: Start Coding!* 🚀 
+
+// Question 55:
+let numbers: number[] = [2, 4, 6, 8];
+let double_numbers = numbers.map((numbers) => numbers * 2);
+console.log(double_numbers);
+
+// Question 56:
+const differentItems = [true, 5, "Handbag", false, 9, "Shoes" ];
+const onlyStrings = differentItems.filter((differentItems) => typeof differentItems === "string")
+console.log(onlyStrings);
+
+// Question 57:
+let grades = [90, 70, 60, 50, 40];
+let average = grades.reduce((total, grades) => total + grades, 0 / grades.length);
+console.log(average);
